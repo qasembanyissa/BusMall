78e1bb3324990e718ff309c3f0a73d002a6aa035
qasembanyissa <q0790405801@gmail.com>
2020-03-03 13:50:32 +0200

    add modify

--- app.js
+++ app.js
@@ -28,26 +28,26 @@ function Product(name, filepath, altText) {
 }
 
 //Make new Products instances
-new Product('Bag', 'img/bag.jpg','Bag');
-new Product('Banana Slicer', 'img/banana.jpg','Banana Slicer');
-new Product('Bathroom', 'img/bathroom.jpg','Bathroom');
-new Product('Boots', 'img/boots.jpg','Boots');
-new Product('Breakfast', 'img/breakfast.jpg','Breakfast');
-new Product('Bubblegum', 'img/bubblegum.jpg', 'Bubblegum');
-new Product('Chair', 'img/chair.jpg', 'Chair');
-new Product('Cthulhu', 'img/cthulhu.jpg', 'Cthulhu');
-new Product('Dog Duck', 'img/dog-duck.jpg', 'Dog Duck');
-new Product('Dragon Meat', 'img/dragon.jpg', 'Dragon Meat');
-new Product('Pen', 'img/pen.jpg', 'Pen');
-new Product('Pet Broom', 'img/pet-sweep.jpg', 'Pet Broom');
-new Product('Scissors', 'img/scissors.jpg', 'Scissors');
-new Product('Shark', 'img/shark.jpg', 'Shark');
-new Product('Sweep', 'img/sweep.png', 'Sweep');
-new Product('Tauntaun', 'img/tauntaun.jpg', 'Tauntaun');
-new Product('Unicorn Meat', 'img/unicorn.jpg', 'Unicorn Meat');
-new Product('Tentacle USB', 'img/usb.gif', 'Tentacle USB');
-new Product('Watering Can', 'img/water-can.jpg', 'Watering Can');
-new Product('Wine Glass', 'img/wine-glass.jpg', 'Wine Glass');
+new Product('Bag', 'images/bag.jpg','Bag');
+new Product('Banana Slicer', 'images/banana.jpg','Banana Slicer');
+new Product('Bathroom', 'images/bathroom.jpg','Bathroom');
+new Product('Boots', 'images/boots.jpg','Boots');
+new Product('Breakfast', 'images/breakfast.jpg','Breakfast');
+new Product('Bubblegum', 'images/bubblegum.jpg', 'Bubblegum');
+new Product('Chair', 'images/chair.jpg', 'Chair');
+new Product('Cthulhu', 'images/cthulhu.jpg', 'Cthulhu');
+new Product('Dog Duck', 'images/dog-duck.jpg', 'Dog Duck');
+new Product('Dragon Meat', 'images/dragon.jpg', 'Dragon Meat');
+new Product('Pen', 'images/pen.jpg', 'Pen');
+new Product('Pet Broom', 'images/pet-sweep.jpg', 'Pet Broom');
+new Product('Scissors', 'images/scissors.jpg', 'Scissors');
+new Product('Shark', 'images/shark.jpg', 'Shark');
+new Product('Sweep', 'images/sweep.png', 'Sweep');
+new Product('Tauntaun', 'images/tauntaun.jpg', 'Tauntaun');
+new Product('Unicorn Meat', 'images/unicorn.jpg', 'Unicorn Meat');
+new Product('Tentacle USB', 'images/usb.gif', 'Tentacle USB');
+new Product('Watering Can', 'images/water-can.jpg', 'Watering Can');
+new Product('Wine Glass', 'images/wine-glass.jpg', 'Wine Glass');
 
 
 //Randomly display products
@@ -175,7 +175,9 @@ var data = {
 function drawChart() {
   var ctx = document.getElementById('productStats').getContext('2d');
   songChart = new Chart(ctx,{
+    // type: 'doughnut',
     type: 'bar',
+
     data: data,
     options: {
       responsive: true,
